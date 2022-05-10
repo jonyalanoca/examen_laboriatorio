7563aecabe8a0860f24c7713c5e578b8d102a190
jonyalanoca <jonyalanoca2@gmail.com>
2022-05-10 16:08:16 -0300

    hola

---
+++ examen/src/examen.c
@@ -0,0 +1,19 @@
+/*
+ ============================================================================
+ Name        : examen.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	setbuf(stdout,NULL);
+	printf("Hola xd\n");
+	system("pause");
+	return EXIT_SUCCESS;
+}
